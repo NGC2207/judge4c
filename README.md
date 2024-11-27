14e8b7a29c2d51af2ebdb32aebf037b8429ba1fd
ngc2207 <cfngc3109@gmail.com>
2024-11-27 13:03:50 +0800

    chore: initialize Next.js project with shadcn

---
+++ README.md
@@ -0,0 +1,14 @@
+# Judge4c
+
+## 📝 Description
+
+A full-stack, open-source online judge platform designed to elevate college programming education
+
+## 🚀 Quick Start
+
+```shell
+git clone https://github.com/NGC2207/judge4c.git
+cd judge4c
+bun install
+bun run dev
+```
---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import "@/app/globals.css";
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+
+export const metadata: Metadata = {
+  title: "Judge4c",
+  description:
+    "A full-stack, open-source online judge platform designed to elevate college programming education",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn("font-sans antialiased", inter.variable)}>
+        {children}
+      </body>
+    </html>
+  );
+}
