a6804cb56d04dc7048838ca960c41be853841bab
ngc2207 <cfngc3109@gmail.com>
2024-11-27 20:32:11 +0800

    feat: add originui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressContentEditableWarning>
+    <html lang="en" suppressHydrationWarning>
       <body className={cn("font-sans antialiased", inter.variable)}>
         <ThemeProvider
           attribute="class"
