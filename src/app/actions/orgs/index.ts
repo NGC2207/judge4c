9f92e133c36df76ac96c0aa0cdbdd049c1e96bd6
ngc2207 <cfngc3109@gmail.com>
2024-11-28 21:00:35 +0800

    feat: enhance organization API calls with improved logging and error handling

--- src/app/actions/orgs/index.ts
+++ src/app/actions/orgs/index.ts
@@ -1,23 +1,59 @@
 "use server";
 
+import {
+  APIError,
+  HttpResponse,
+  Organization,
+  RequestParams,
+  CreateOrgOption,
+} from "gitea-js";
 import api from "@/lib/gitea";
 import callApi from "@/lib/api";
-import { CreateOrgOption, Organization } from "gitea-js";
+import logger from "@/lib/logger";
 
-// /orgs
-// get
-// Get list of organizations
-export async function orgsGet(): Promise<Organization[]> {
-  const response = await callApi(() => api.orgs.orgGetAll());
-  return response.data;
+export async function orgGetAll(
+  query?: {
+    /** page number of results to return (1-based) */
+    page?: number;
+    /** page size of results */
+    limit?: number;
+  },
+  params?: RequestParams
+): Promise<HttpResponse<Organization[], APIError>> {
+  const context = { query, params };
+  logger.info("Fetching organizations...", context);
+  const result = await callApi(
+    () => api.orgs.orgGetAll(query, params),
+    "List of organizations retrived",
+    context
+  );
+
+  if (result.error) {
+    logger.error("Failed to fetch organizations", result.error);
+    return result.error as HttpResponse<Organization[], APIError>;
+  }
+
+  logger.info("Organizations fetched sucessfully", result.data);
+  return result.data as HttpResponse<Organization[], APIError>;
 }
 
-// /orgs
-// post
-// Create an organization
-export async function orgsPost(
-  createOrgOption: CreateOrgOption
-): Promise<Organization> {
-  const response = await callApi(() => api.orgs.orgCreate(createOrgOption));
-  return response.data;
+export async function orgCreate(
+  organization: CreateOrgOption,
+  params?: RequestParams
+): Promise<HttpResponse<Organization, APIError>> {
+  const context = { organization, params };
+  logger.info({ context }, "Creating organization...");
+  const result = await callApi(
+    () => api.orgs.orgCreate(organization, params),
+    "Organization created",
+    context
+  );
+
+  if (result.error) {
+    logger.error({ error: result.error }, "Failed to create organization");
+    return result.error as HttpResponse<Organization, APIError>;
+  }
+
+  logger.info({ data: result.data }, "Organization created sucessfully");
+  return result.data as HttpResponse<Organization, APIError>;
 }
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,14 +1,27 @@
 import logger from "./logger";
+import { APIError } from "gitea-js";
 
-async function callApi<T>(apiCall: () => Promise<T>): Promise<T> {
+async function callApi<T>(
+  apiCall: () => Promise<T>,
+  successMessage: string,
+  context?: Record<string, unknown>
+): Promise<{ data?: T; error?: APIError }> {
   try {
-    return await apiCall();
+    logger.info("Calling Gitea API...");
+    const response = await apiCall();
+    logger.info(successMessage, response);
+    return { data: response };
   } catch (error) {
-    logger.error("API Error", {
-      message: (error as Error).message,
-      stack: (error as Error).stack,
-    });
-    throw error;
+    const giteaAPIError = error as { error: APIError };
+    logger.error(
+      {
+        error: giteaAPIError.error.message,
+        url: giteaAPIError.error.url,
+        context,
+      },
+      "API Error"
+    );
+    return { error: giteaAPIError.error };
   }
 }
 
