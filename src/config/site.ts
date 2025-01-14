0a5e818826e8b932753e0de9cc8b5bd510bd1304
ngc2207 <cfngc3109@gmail.com>
2025-01-14 19:05:51 +0800

    chore(config): update site configuration and layout metadata

---
+++ src/config/site.ts
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  title: "Judge4c",
+  description:
+    "A full-stack, open-source online judge platform designed to elevate college programming education",
+  url: {
+    repo: {
+      github: "https://github.com/NGC2207/judge4c",
+    },
+  },
+  author: {
+    name: "NGC2207",
+    links: {
+      github: "https://github.com/NGC2207",
+    },
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
