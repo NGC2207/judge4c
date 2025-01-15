a3eab03631049d95fcf624920f749f949cba9df7
ngc2207 <cfngc3109@gmail.com>
2025-01-15 20:24:30 +0800

    feat(auth): add GitHub provider for authentication and configure remote image patterns

--- src/auth.ts
+++ src/auth.ts
@@ -1,8 +1,9 @@
 import NextAuth from "next-auth";
 import { prisma } from "@/lib/prisma";
+import GitHub from "next-auth/providers/github";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [GitHub],
 });
